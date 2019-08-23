37c40606d60cb8256c8c1bc649ceaeac6d54bf0e
Enrique C <ecoronado@scouting.org>
2019-08-23 12:47:50 -0500

    Playing with React Thunks
    Project to play around with react thunks using tweets

---
+++ src/reducer.js
@@ -0,0 +1,13 @@
+import { combineReducers } from 'redux';
+import { ADD_TWEETS } from './actions';
+
+const tweets = (tweets = [], action) => {
+  if (action.type === ADD_TWEETS) {
+    return action.payload.tweets;
+  }
+  return tweets;
+};
+
+export default combineReducers({
+  tweets,
+});
